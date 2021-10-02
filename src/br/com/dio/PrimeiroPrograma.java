2eda5280047635a3f126b31fe2cce90f6b762a2a
Anailton <anailtonascimento@gmail.com>
2021-10-02 20:49:52 -0300

    Second commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,37 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos três corpos", 300);
+        System.out.println(livro1);
 
        /* int a = 5;
         int b = 6;
         System.out.println("Heloo world!" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
